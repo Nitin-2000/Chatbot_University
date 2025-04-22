a33585290a8dc813a05fd783b1d124b8c91a83d6
Nitin Yadav <officialnitin20@gmail.com>
2025-04-22 12:39:24 +0530

    removed link response

--- src/App.tsx
+++ src/App.tsx
@@ -500,7 +500,7 @@ function App() {
           <button
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 w-full"
             onClick={() => {
-              handleSendMessage("CET");
+              handleSendMessage("CDOE Distance Education");
               window.open(
                 "https://mu.ac.in/distance-open-learning",
                 "_blank",
